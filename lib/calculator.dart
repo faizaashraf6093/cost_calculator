79e3c98bc77dd5aa785559125e19ab064c5680d3
faizaashraf6093 <faizaashraf0786@gmail.com>
2023-11-30 01:33:51 +0500

    ui customization

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -2,16 +2,12 @@ import 'package:flutter/material.dart';
 
 class Calculator extends StatefulWidget {
   const Calculator({super.key});
-
   @override
   State<Calculator> createState() => _CalculatorState();
 }
-
 class _CalculatorState extends State<Calculator> {
   String? selectedCategory;
-
   Map<String, int> selectedRoles = {};
-
   int projectDuration = 1;
 
   final Map<String, List<String>> _categories = {
@@ -20,7 +16,7 @@ class _CalculatorState extends State<Calculator> {
       'Back end engineer',
       'Solution Architect',
       'Project manager',
-      'Designer',
+      'Designer'
     ],
     // Add more categories here
   };
@@ -28,120 +24,119 @@ class _CalculatorState extends State<Calculator> {
   final Map<String, int> _experienceLevels = {
     'Junior': 5000,
     'Mid-level': 7000,
-    'Senior': 9000,
+    'Senior': 9000
   };
 
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          DropdownButton<String>(
-            hint: const Text('Select Category'),
-            value: selectedCategory,
-            onChanged: (value) {
-              setState(() {
-                selectedCategory = value;
-              });
-            },
-            items: _categories.keys.map((String category) {
-              return DropdownMenuItem<String>(
-                value: category,
-                child: Text(category),
-              );
-            }).toList(),
-          ),
-          const Text(
-            'Select Roles:',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Wrap(
-            children: _categories[selectedCategory]?.map((String role) {
-                  int teamSize = selectedRoles.containsKey(role)
-                      ? selectedRoles[role]!
-                      : 0;
+      body: Center(
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: [
+            _buildDropdown('Select Category', _categories.keys,
+                (value) => setState(() => selectedCategory = value)),
+            Center(child: _buildRoleSelection(height)),
+            const SizedBox(height: 16.0),
+            _buildDropdown(
+                'Select Project Duration (months)',
+                ['1', '2', '3', '4', '5'],
+                (value) => setState(
+                    () => projectDuration = int.tryParse(value!) ?? 1)),
+            const SizedBox(height: 16.0),
+            _buildEstimatedBudget(),
+          ],
+        ),
+      ),
+    );
+  }
 
-                  return Container(
-                    width: 280,
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 8.0, vertical: 8.0),
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: Row(
-                      children: [
-                        IconButton(
-                            onPressed: () {
-                              _updateTeamSize(role, teamSize - 1);
-                            },
-                            icon: const Icon(Icons.remove)),
-                        Text('$role ($teamSize)'),
-                        IconButton(
-                            onPressed: () {
-                              _updateTeamSize(role, teamSize + 1);
-                            },
-                            icon: const Icon(Icons.add)),
-                      ],
-                    ),
-                  );
-                }).toList() ??
-                [],
-          ),
-          _buildDropdown(
-            label: 'Select Project Duration (months)',
-            value: projectDuration.toString(),
-            onChanged: (value) {
-              setState(() {
-                projectDuration = int.tryParse(value!) ?? 1;
-              });
-            },
-            items: ['1', '2', '3', '4', '5'].toList(),
-          ),
-          const SizedBox(height: 16.0),
-          const Text('Estimated Budget'),
-          Text('\$${_calculateBudget()}'),
+  Widget _buildDropdown(
+      String label, Iterable<String> items, void Function(String?) onChanged) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(label, style: const TextStyle(fontWeight: FontWeight.bold)),
+        DropdownButton<String>(
+          alignment: Alignment.center,
+          focusColor: Colors.transparent,
+          hint: Text('Select $label'),
+          value: label == 'Select Category'
+              ? selectedCategory
+              : projectDuration.toString(),
+          onChanged: onChanged,
+          items: items
+              .toList()
+              .map((item) =>
+                  DropdownMenuItem<String>(value: item, child: Text(item)))
+              .toList(),
+        ),
+        const SizedBox(height: 16.0),
+      ],
+    );
+  }
+
+  Widget _buildRoleSelection(double height) {
+    return selectedCategory != null && selectedCategory != ''
+        ? Wrap(
+          children: _categories[selectedCategory!]
+                  ?.map((role) => _buildRoleContainer(role))
+                  .toList() ??
+              [],
+        )
+        : SizedBox(
+            height: height * 0.4,
+            child: const Center(
+              child: Text(
+                'Please select any category',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              ),
+            ),
+          );
+  }
+
+  Widget _buildRoleContainer(String role) {
+    int teamSize = selectedRoles.containsKey(role) ? selectedRoles[role]! : 0;
+    return Container(
+      width: 280,
+      margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+              onPressed: () => _updateTeamSize(role, teamSize - 1),
+              icon: const Icon(Icons.remove)),
+          Text('$role ($teamSize)'),
+          IconButton(
+              onPressed: () => _updateTeamSize(role, teamSize + 1),
+              icon: const Icon(Icons.add)),
         ],
       ),
     );
   }
 
-  void _updateTeamSize(String role, int teamSize) {
-    setState(() {
-      selectedRoles = {...selectedRoles, role: teamSize};
-    });
-  }
-
-  Widget _buildDropdown({
-    required String label,
-    required String? value,
-    required Function(String?) onChanged,
-    required List<String> items,
-  }) {
+  Widget _buildEstimatedBudget() {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          label,
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        ),
-        DropdownButton<String>(
-          hint: Text('Select $label'),
-          value: value,
-          onChanged: onChanged,
-          items: items.map((String item) {
-            return DropdownMenuItem<String>(
-              value: item,
-              child: Text(item),
-            );
-          }).toList(),
-        ),
-        const SizedBox(height: 16.0),
+        const Text('Estimated Budget',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        Text('\$${_calculateBudget()}',
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
       ],
     );
+  }
+
+  void _updateTeamSize(String role, int teamSize) {
+    if (teamSize >= 0) {
+      setState(() => selectedRoles = {...selectedRoles, role: teamSize});
+    }
   }
 
   int _calculateBudget() {
@@ -154,12 +149,9 @@ class _CalculatorState extends State<Calculator> {
     return totalBudget;
   }
 
-  String _getExperienceLevel(String role) {
-    return selectedRoles.containsKey(role)
-        ? _experienceLevels.keys.firstWhere(
-            (level) => _experienceLevels[level] == _experienceLevels[role],
-            orElse: () => 'Junior',
-          )
-        : 'Junior';
-  }
+  String _getExperienceLevel(String role) => selectedRoles.containsKey(role)
+      ? _experienceLevels.keys.firstWhere(
+          (level) => _experienceLevels[level] == _experienceLevels[role],
+          orElse: () => 'Junior')
+      : 'Junior';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cost_calculator/home.dart';
+import 'package:cost_calculator/calculator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const Calculator(),
     );
   }
 }
